441596cdbf7afb8211f756c299750c7390647f73
FreskyZ <FreskyZ@outlook.com>
2022-09-22 23:52:36 +0800

    [core] add basic send group message

--- src/ui/index.tsx
+++ src/ui/index.tsx
@@ -1,11 +1,10 @@
 // @ts-ignore
-import React, { useReducer } from 'react';
+import React from 'react';
 import { useState, useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom';
-import { Select, Button } from 'antd';
-import dayjs from 'dayjs';
-import * as api from '../api-decl';
-import { Message } from '../api-decl/types';
+import { ConfigProvider, Select, Button, Input, message } from 'antd';
+import { $default as api } from '../api/client';
+import { Message } from '../api/types';
 
 // very primitive technology to add link to image
 function MessageContent({ content }: { content: string }) {
@@ -17,7 +16,7 @@ function MessageContent({ content }: { content: string }) {
             const match = replacere.exec(seg);
             return match ? <>
                 {match[1] == 'image'
-                    ? <img src={match[3]} referrerPolicy='no-referrer' />
+                    ? <a href={match[3]} target='_blank' referrerPolicy='no-referrer'><img src={match[3]} referrerPolicy='no-referrer' /></a>
                     : <a href={match[3].replaceAll(/* video message unexpectedly included the &amp */'&' + 'amp;', '&')} target='_blank' referrerPolicy='no-referrer'>{match[2]}</a>}
             </> : <span>{seg}</span>;
         })}
@@ -37,11 +36,11 @@ function App() {
     const [group, setGroup] = useState(0);
     const [groups, setGroups] = useState<number[]>([0]);
     const [messages, setMessages] = useState<Message[]>([]);
-    const [time, setTime] = useState<string>();
+    const [editingMessage, setEditingMessage] = useState<string>("");
     const callbackref = useRef<() => void>();
 
     useEffect(() => {
-        api.$default.getRecentGroups().then(groups => setGroups(groups));
+        api.getRecentGroups().then(groups => setGroups(groups));
     }, []);
 
     useEffect(() => {
@@ -53,21 +52,24 @@ function App() {
     }, [group]);
 
     function handleRefresh() {
-        if (group != 0) {
-            api.$default.getGroupRecentMessages(group).then(messages => setMessages(messages));
+        if (group != 0 && document.visibilityState == 'visible') {
+            api.getGroupRecentMessages(group).then(messages => setMessages(messages));
         }
     }
     callbackref.current = handleRefresh;
 
     useEffect(() => {
-        const id = setInterval(() => callbackref.current(), 10000);
+        const id = setInterval(() => callbackref.current(), 12000);
         return () => clearInterval(id);
     }, []);
 
-    useEffect(() => {
-        const id = setInterval(() => setTime(dayjs().format('HH:mm:ss')), 1000);
-        return () => clearInterval(id);
-    }, []);
+    function handleSend() {
+        if (editingMessage.trim().length > 0) {
+            api.sendGroupMessage({ groupId: group, content: editingMessage.trim() }).then(() => {
+                setEditingMessage("");
+            }, ex => { message.error(ex); });
+        }
+    }
 
     return <>
         <header>WACQ</header>
@@ -75,10 +77,13 @@ function App() {
             {groups.map(g => <Select.Option key={g} value={g}>{g}</Select.Option>)}
         </Select>
         <Button className='refresh' onClick={() => handleRefresh()}>刷新</Button>
-        <span>{time}</span>
+        <div className='send-container'>
+            <Input value={editingMessage} onChange={e => setEditingMessage(e.target.value)} onPressEnter={handleSend} />
+            <Button disabled={editingMessage.trim().length == 0} onClick={handleSend}>发送</Button>
+        </div>
         <Messages messages={messages} />
     </>;
 }
 
 // antd@4 does not support react@18, for now
-ReactDOM.render(<App />, document.querySelector('main'));
+ReactDOM.render(<ConfigProvider autoInsertSpaceInButton={false}><App /></ConfigProvider>, document.querySelector('main'));
